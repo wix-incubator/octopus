410b7e3cc2d6a38652dd6297877ae39965cba20e
itaipelles <itaip@wix.com>
2017-02-26 17:54:03 +0200

    self update does not need to use 'forCommand'

--- cli/scripts/commands/octo-selfupdate.js
+++ cli/scripts/commands/octo-selfupdate.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
-const forCommand = require('../../lib/commands').forCommand,
+const log = require('../../lib/logger')(),
   shelljs = require('shelljs');
 
 exports.command = 'selfupdate';
 exports.desc = 'npm install -g latest version of myself';
 exports.builder = yargs => yargs.usage('\nUsage: octo selfupdate');
 
-exports.handler = forCommand('octo selfupdate (npm install -g octopus-cli)', () => {
-  shelljs.exec('npm install -g octopus-cli', {silent: false});
-});
+exports.handler = () => {
+  log.exec('octo selfupdate (npm install -g octopus-cli)', () => {
+    shelljs.exec('npm install -g octopus-cli', {silent: false});
+  });
+};
