cf926c2f11efca0472867306df49a4e721f87b96
Vilius Lukosius <vilius@wix.com>
2017-04-09 18:31:56 +0300

    Updated test

--- start-reporter/index.js
+++ start-reporter/index.js
@@ -3,6 +3,6 @@ module.exports = (out = console) => (name, type, message) => {
   if (type === 'info' && typeof message !== 'undefined') {
     out.log(`[${name}]: ${message}`);
   } else if (type === 'reject') {
-    out.log(`[${name}]: failed with ${message.stack}`);
+    out.error(`[${name}]: failed with ${message.stack}`);
   }
 };
--- start-reporter/test/reporter.spec.js
+++ start-reporter/test/reporter.spec.js
@@ -30,7 +30,7 @@ describe('reporter', () => {
     const out = mockConsole();
     reporter(out)('fnName', 'reject', new Error('qwe'));
 
-    expect(out.log).to.have.been.calledWith(sinon.match('[fnName]: failed with Error: qwe'));
+    expect(out.error).to.have.been.calledWith(sinon.match('[fnName]: failed with Error: qwe'));
   });
 
   function mockConsole() {
