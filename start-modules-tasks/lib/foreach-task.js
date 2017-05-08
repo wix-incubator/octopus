78717c9bfd7acf476a6404e6a8673a3545d7121e
Vilius Lukosius <vilius@wix.com>
2017-05-08 17:04:23 +0300

    start-modules-tasks: simplified foreach-task

--- start-modules-tasks/lib/foreach-task.js
+++ start-modules-tasks/lib/foreach-task.js
@@ -4,13 +4,12 @@ const defaults = {mapInput: input => input, silent: false};
 
 module.exports = ({mapInput = defaults.mapInput, silent = defaults.silent} = defaults) => fn => taskInput => {
   return function forEachModules(log, reporter) {
-    const results = [];
     return Promise.each(mapInput(taskInput), (item, index, length) => {
       return Promise.resolve().then(() => {
         if (!silent) {
           log(`${item.name} (${item.relativePath}) (${index + 1}/${length})`);
         }
-        return Promise.resolve().then(() => fn(item, taskInput, reporter)).then(res => results.push(res));
+        return Promise.method(fn)(item, taskInput, reporter);
       });
     }).then(() => taskInput);
   };
