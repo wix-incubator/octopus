3990f4844742a662899f4ae929e407174f0a6be9
Vilius Lukosius <vilius@wix.com>
2017-05-23 00:01:21 +0300

    More extraction

--- start-preset-dependencies/lib/unmanaged.js
+++ start-preset-dependencies/lib/unmanaged.js
@@ -12,47 +12,69 @@ function unmanagedDependenciesTask() {
 
     return start(reporter)(
       modules.load(),
-      iter.async({silent: true})((module, input, asyncReporter) => {
-        const readPackageJson = readJson(module)('package.json');
-        const fillModulesAndDeps = packageJson => () => {
-          return Promise.resolve().then(() => {
-            innerModules.push(packageJson.name);
-            fill(deps.dependencies)(packageJson, 'dependencies');
-            fill(deps.dependencies)(packageJson, 'devDependencies');
-            fill(deps.peerDependencies)(packageJson, 'peerDependencies');
-          });
-        };
-        return start(asyncReporter)(readPackageJson, fillModulesAndDeps);
-      }),
+      iter.async({silent: true})((module, input, asyncReporter) =>
+        start(asyncReporter)(
+          readJson(module)('package.json'),
+          fillModulesAndDeps(innerModules, deps))
+      ),
       startTasks.readJson('package.json'),
-      ({managedDependencies = {}, managedPeerDependencies = {}}) => (log/*, reporter*/) => {
-        return Promise.resolve().then(() => {
-          innerModules.forEach(name => delete deps.dependencies[name]);
-          innerModules.forEach(name => delete deps.peerDependencies[name]);
-          Object.keys(managedDependencies).forEach(name => delete deps.dependencies[name]);
-          Object.keys(managedPeerDependencies).forEach(name => delete deps.peerDependencies[name]);
-
-          Object.keys(deps.dependencies).forEach(depKey => {
-            const modulesAndVersions = Object.keys(deps.dependencies[depKey]).map(module => `${module} (${deps.dependencies[depKey][module]})`);
-            log(`Unmanaged dependency ${depKey} in ${modulesAndVersions.join(', ')}`);
-          });
-
-          Object.keys(deps.peerDependencies).forEach(depKey => {
-            const modulesAndVersions = Object.keys(deps.peerDependencies[depKey]).map(module => `${module} (${deps.peerDependencies[depKey][module]})`);
-            log(`Unmanaged peerDependency ${depKey} in ${modulesAndVersions.join(', ')}`);
-          });
-
-
-          if ((Object.keys(deps.dependencies).length + Object.keys(deps.peerDependencies).length) > 0) {
-            return Promise.reject(new Error('Unmanaged dependencies found, see output above'));
-          } else {
-            return Promise.resolve();
-          }
-        });
-      }
+      executeUnmanaged(innerModules, deps)
     )
   }
 }
+
+function executeUnmanaged(innerModules, deps) {
+  return ({managedDependencies = {}, managedPeerDependencies = {}}) => {
+    return (log/*, reporter*/) => {
+      cleanProjectDeps(innerModules, deps);
+      cleanManagedDeps(deps, managedDependencies, managedPeerDependencies);
+      logUnmanaged(deps, log);
+      return rejectIfUnmanaged(deps);
+    };
+  }
+}
+
+function logUnmanaged(deps, log) {
+  Object.keys(deps.dependencies).forEach(depKey => {
+    const modulesAndVersions = Object.keys(deps.dependencies[depKey]).map(module => `${module} (${deps.dependencies[depKey][module]})`);
+    log(`Unmanaged dependency ${depKey} in ${modulesAndVersions.join(', ')}`);
+  });
+
+  Object.keys(deps.peerDependencies).forEach(depKey => {
+    const modulesAndVersions = Object.keys(deps.peerDependencies[depKey]).map(module => `${module} (${deps.peerDependencies[depKey][module]})`);
+    log(`Unmanaged peerDependency ${depKey} in ${modulesAndVersions.join(', ')}`);
+  });
+}
+
+function rejectIfUnmanaged(deps) {
+  if ((Object.keys(deps.dependencies).length + Object.keys(deps.peerDependencies).length) > 0) {
+    return Promise.reject(new Error('Unmanaged dependencies found, see output above'));
+  } else {
+    return Promise.resolve();
+  }
+}
+
+function cleanProjectDeps(innerModules, deps) {
+  innerModules.forEach(name => delete deps.dependencies[name]);
+  innerModules.forEach(name => delete deps.peerDependencies[name]);
+}
+
+function cleanManagedDeps(deps, managedDependencies, managedPeerDependencies) {
+  Object.keys(managedDependencies).forEach(name => delete deps.dependencies[name]);
+  Object.keys(managedPeerDependencies).forEach(name => delete deps.peerDependencies[name]);
+}
+
+function fillModulesAndDeps(innerModules, deps) {
+  return packageJson => () => {
+    return Promise.resolve().then(() => {
+      innerModules.push(packageJson.name);
+      fill(deps.dependencies)(packageJson, 'dependencies');
+      fill(deps.dependencies)(packageJson, 'devDependencies');
+      fill(deps.peerDependencies)(packageJson, 'peerDependencies');
+    });
+  };
+}
+
 
 function fill(deps) {
   return (packageJson, type) => {
